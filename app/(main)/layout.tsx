81a6e13886f1038b6e0d9fe467db084c85c19d2b
gabbramis <gabrielaramis01@gmail.com>
2025-09-10 23:44:17 -0300

    google analytics

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,5 +1,6 @@
 import Footer from "@/components/home/Footer";
 import Header from "@/components/home/Header";
+import Script from "next/script";
 
 export default function MainLayout({
   children,
@@ -11,6 +12,9 @@ export default function MainLayout({
       <Header />
       {children}
       <Footer />
+      {process.env.NODE_ENV === "production" && (
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-MMWKS311JX" />
+      )}
     </>
   );
 }
